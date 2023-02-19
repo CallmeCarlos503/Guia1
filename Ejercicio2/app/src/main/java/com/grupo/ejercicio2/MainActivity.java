48c810fe0210ee1bb6af3f63e1f056d7407032de
Carlos_Alberto_Sanchez <95440504+CallmeCarlos503@users.noreply.github.com>
2023-02-19 13:53:02 -0600

    ARREGLAO

--- Ejercicio2/app/src/main/java/com/grupo/ejercicio2/MainActivity.java
+++ Ejercicio2/app/src/main/java/com/grupo/ejercicio2/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
     EditText Precio;
     Button btnCalcular;
@@ -21,12 +23,13 @@ public class MainActivity extends AppCompatActivity {
     }
     public void Calculo(View view ){
         Double precio,total;
+        DecimalFormat formato1 = new DecimalFormat("#.00"); //! limitar decimales a 2 o mas
         try{
             precio=Double.parseDouble(Precio.getText().toString());
-            Total.setText("IVA: 13.4%: "+(precio*0.134)+"\n Total con el racargo: "+(precio+(precio*0.134)));
+            Total.setText("IVA: 13.4%:"+formato1.format(precio*0.134)+"\n Total con el racargo: "+ formato1.format(precio+(precio*0.134)));
         }
         catch(Exception ex){
-            Total.setText("Error: "+ex);
+            Total.setText("Error, porfavor asigne un numero ");
         }
     }
 }
